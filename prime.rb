0fcabeac9cc25a86e10bf246bc96532eb9e5788d
Scott Harrison <s.z.r.harrison@gmail.com>
2017-03-07 00:21:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(n)
+  n = n
+  if ( n <= 1 )
+    is_p = false
+  elsif ( n == 2 || n == 3 )
+    is_p = true
+  else
+    for d in 2..Math.sqrt( n )
+      if ( n % d == 0 )
+        break is_p = false
+      else
+        is_p = true
+      end
+    end
+  end
+  return is_p
+end
+
+prime?(40)
